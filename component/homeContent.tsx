0944a6d6fdb35ff8536287ee9d856e10aca8c6e6
shubham negi <shubham.negi@bitcs.in>
2022-05-31 20:52:47 +0530

    fixed bug

--- component/homeContent.tsx
+++ component/homeContent.tsx
@@ -35,7 +35,7 @@ export const HomeContent = () => {
        */}
       <div className={styles.midContainer}>
         <h1>A survey tool like no other</h1>
-        <p style={{ fontSize: "78px" }}>
+        <p>
           Only Survey Rocketo connects you to valuable customer insights—and
           then helps you turn those insights into action, automatically.
         </p>
